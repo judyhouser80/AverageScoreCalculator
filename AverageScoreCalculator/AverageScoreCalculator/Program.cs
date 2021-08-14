046020e0739d2bda03d92da00e1674d1dfce73fc
Judyhouser80 <judyhouser80@gmail.com>
2021-08-14 13:02:44 -0400

    Final changes

--- AverageScoreCalculator/AverageScoreCalculator/Program.cs
+++ AverageScoreCalculator/AverageScoreCalculator/Program.cs
@@ -28,8 +28,9 @@ namespace AverageScoreCalculator
                 if(input == "-1")
                 {
                     Console.WriteLine("---------");
-                    
-                    //Calculate average.
+
+                    double average = (double)total / (double)count;
+                    Console.WriteLine("The average score of your students is {0}", average);
                 }
 
                 if(int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber < 101)
@@ -44,24 +45,11 @@ namespace AverageScoreCalculator
                     }
                     continue;
                 }
-            }    
 
-            /*double studentScore1, studentScore2, studentScore3, studentScore4;
+                count++;
+            }
 
-            Console.WriteLine("Enter first student score:  ");
-            studentScore1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter second student score:  ");
-            studentScore2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter third student score:  ");
-            studentScore3 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter fourth student score:  ");
-            studentScore4 = Convert.ToDouble(Console.ReadLine());
-
-            double result = (studentScore1 + studentScore2 + studentScore3 + studentScore4) / 4;
-            Console.WriteLine("The average student score of {0}, {1}, {2}, {3}, is:  {4}", studentScore1, studentScore2, studentScore3, studentScore4, result); */
+            Console.ReadLine();
         }
     }
 }
