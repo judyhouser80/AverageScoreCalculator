ff04d9c76f165ac4ca0ff02170ea150a3fff0b1b
Judyhouser80 <judyhouser80@gmail.com>
2021-08-14 08:56:36 -0400

    Initial Commit

---
+++ AverageScoreCalculator/AverageScoreCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AverageScoreCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
