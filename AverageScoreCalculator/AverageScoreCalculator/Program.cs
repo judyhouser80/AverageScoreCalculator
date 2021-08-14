d219933d2df1eaa0a4726f35377507d228d98b65
Judyhouser80 <judyhouser80@gmail.com>
2021-08-14 09:16:50 -0400

    basic code added

--- AverageScoreCalculator/AverageScoreCalculator/Program.cs
+++ AverageScoreCalculator/AverageScoreCalculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AverageScoreCalculator
 {
@@ -6,7 +7,27 @@ namespace AverageScoreCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //average score calculator
+            //make sure program does not crash when something other than a number is added.
+            //make sure program does not crash when a number not in range is added.
+
+            double studentScore1, studentScore2, studentScore3, studentScore4;
+
+            Console.WriteLine("Enter first student score:  ");
+            studentScore1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second student score:  ");
+            studentScore2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter third student score:  ");
+            studentScore3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter fourth student score:  ");
+            studentScore4 = Convert.ToDouble(Console.ReadLine());
+
+            double result = (studentScore1 + studentScore2 + studentScore3 + studentScore4) / 4;
+            Console.WriteLine("The average student score of {0}, {1}, {2}, {3}, is:  {4}", studentScore1, studentScore2, studentScore3, studentScore4, result);
         }
     }
 }
